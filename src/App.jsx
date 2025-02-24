908bcb0394f97d8c3d7d160b7c5c7366e2969029
diegoohav <velizdiego336@gmail.com>
2025-02-24 10:14:57 -0300

    creando resultado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Titulo } from './components/Titulo'
+import { Resultado } from './components/Resultado'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Titulo titulo={"Titulo de pagina"}/>
+      <Resultado numeroa={5} numerob={7}/>
     </>
   )
 }
---
+++ src/components/Resultado.jsx
@@ -0,0 +1,6 @@
+export const Resultado = ({numeroa,numerob})=>{
+    return <>
+    <h3>Resultado: {numeroa+numerob} </h3>
+
+    </>
+}
